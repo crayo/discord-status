f52d9a0468a25fabf22bbd26513f82e58d669824
Koy N Kragh <koynkragh@gmail.com>
2018-07-02 00:09:42 -0500

    update to WATCHING, minor tweaks

--- src/app.js
+++ src/app.js
@@ -5,8 +5,9 @@ import tasklist from 'tasklist';
 const userToken = process.env.DISCORD_USER_TOKEN || '';
 const updateInterval = process.env.DISCORD_STATUS_UPDATE_INTERVAL || 10 * 1000;
 const mpvTitlePattern = /^\[mpv\] (?:file: )?(.+)$/;
-
 const client = new Discord.Client();
+
+let updateTimeout = null;
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -23,38 +24,43 @@ const findMPVWindowTitle = async () => {
 }
 
 const checkUpdateStatus = async () => {
+  const clientStatus = _.get(client, 'user.presence.game.name', '');
   const mpvTitle = await findMPVWindowTitle();
   const titleMatch = mpvTitle.match(mpvTitlePattern);
-  const clientStatus = _.get(client, 'user.presence.game.name', '');
-  let mediaStatus = '';
-  if (titleMatch && titleMatch[1] && titleMatch[1] !== 'No file') {
-    console.log(`found media: ${titleMatch[1]}`);
-    mediaStatus = titleMatch[1]
-      .replace(/\[[^\]]*\]/g, '')
-      .replace(/\([^\)]*(720|1080)[^\)]*\)/gi, '')
-      .replace(/(\s|_)/g, ' ')
-      .trim()
-    ;
-  }
-
-  console.log('Parsed:');
-  console.log(`  media : "${mediaStatus}"`);
-  console.log(`  client: "${clientStatus}"`);
+  const mediaStatus = _.get(titleMatch, '[1]', '')
+    .replace(/\[[^\]]*\]/g, '')
+    .replace(/\([^\)]*(720|1080)[^\)]*\)/gi, '')
+    .replace(/(\s|_)/g, ' ')
+    .trim()
+  ;
 
   if (mediaStatus && (mediaStatus !== clientStatus)) {
     // update status
-    console.log('  updating to media status');
-    client.user.setPresence({ game: { name: mediaStatus } });
+    console.log(`updating status: ${mediaStatus}`);
+    client.user.setPresence({ game: { name: mediaStatus, type: 'WATCHING' } });
   } else if (!mediaStatus && clientStatus) {
     // clear status
-    console.log('  clearing status');
+    console.log('clearing status');
     client.user.setPresence({ game: null });
-  } else {
-    console.log('  doing nothing');
   }
 
-  client.setTimeout(checkUpdateStatus, updateInterval);
+  updateTimeout = client.setTimeout(checkUpdateStatus, updateInterval);
 }
+
+process.on('SIGINT', () => {
+  console.log('caught SIGINT');
+  if (updateTimeout) {
+    console.log('clearing timeout');
+    client.clearTimeout(updateTimeout);
+  }
+  const clientStatus = _.get(client, 'user.presence.game.name', '');
+  if (clientStatus) {
+    console.log('clearing status');
+    client.user.setPresence({ game: null });
+  }
+  console.log('exiting');
+  process.exit();
+});
 
 if (! userToken) {
   console.log('you must supply a user token in the DISCORD_USER_TOKEN environment variable');
